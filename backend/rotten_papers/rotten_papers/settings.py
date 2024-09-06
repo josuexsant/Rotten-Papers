ac8455f0fd31eb5a15f46dd13dca19ec48ab3e87
gab <gabcam071118.rg@gmail.com>
2024-09-06 11:16:33 -0600

    Configuración rest_framework y conexión a BD rotten-papers

--- backend/rotten_papers/rotten_papers/settings.py
+++ backend/rotten_papers/rotten_papers/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'rp_app',
 ]
 
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'rotten_papers.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'rotten_papers',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
