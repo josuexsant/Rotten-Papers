3636529161a6bc72de257b3a144fd27c5d265a2f
gab <gabcam071118.rg@gmail.com>
2024-09-18 13:06:23 -0600

    Preparación al backend

--- backend/rotten_papers/rp_app/admin.py
+++ backend/rotten_papers/rp_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Libros, Autores, Genero, Librogenero        
 
 # Register your models here.
+admin.site.register(Libros)
+admin.site.register(Autores)
+admin.site.register(Genero)
+admin.site.register(Librogenero)
