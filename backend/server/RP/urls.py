2ba85c42a05f01095bddddefe8c55db1bb06f991
gab <gabcam071118.rg@gmail.com>
2025-05-08 19:12:06 -0600

    Add shopping cart routes to URLs

--- backend/server/RP/urls.py
+++ backend/server/RP/urls.py
@@ -18,4 +18,6 @@ urlpatterns = [
     re_path('book', views.book),
     re_path('author', views.author),
     re_path('editProfile', views.editProfile),
+    re_path('shoppingCar', views.shopping_cart),
+    re_path('shoppingCartBooks', views.clear_shopping_cart),
 ]
